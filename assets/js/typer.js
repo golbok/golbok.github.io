c883d20423043a527ad5c6b420090b3b9dc7cfdd
golbok <i.mail@tuta.io>
2017-08-08 07:09:47 -0400

    dont judge my assets :)

--- assets/js/typer.js
+++
@@ -1,1 +0,0 @@
-var str="<p><h2>$ Hallo !</h2></p>",i=0,isTag,text;!function t(){if((text=str.slice(0,++i))!==str){document.getElementById("typewriter").innerHTML=text;var e=text.slice(-1);if("<"===e&&(isTag=!0),">"===e&&(isTag=!1),isTag)return t();setTimeout(t,80)}}();
